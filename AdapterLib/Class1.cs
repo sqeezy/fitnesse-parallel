a77b6765cd1abb632fef2f71aef61d05f773bd32
Anton Herzog <info@antonherzog.com>
2019-12-03 06:55:40 +0100

    Add fitnesse wiki and config file

--- AdapterLib/Class1.cs
+++ AdapterLib/Class1.cs
@@ -2,7 +2,14 @@
 
 namespace AdapterLib
 {
-    public class Class1
+    public class Adapter
     {
+        public double A {get;set;}
+        public double B {get;set;}
+
+        public double Mul => A * B;
+        public double Add => A * B;
+        public double Sub => A * B;
+        public double Div => A * B;
     }
 }
