93ff66180822eb24aebff56cda40c2a998cda6c2
Anton Herzog <info@antonherzog.com>
2019-12-04 08:24:00 +0100

    Create minimal sample for problem.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/dotnet/framework/runtime:4.8
+FROM mcr.microsoft.com/dotnet/framework/sdk:4.8
 
 SHELL ["powershell", "-Command", "$ErrorActionPreference = 'Stop'; $ProgressPreference = 'SilentlyContinue';"]
 
--- Run-FitnesseWithNewUpdatedLib.ps1
+++ Run-FitnesseWithNewUpdatedLib.ps1
@@ -1,2 +1,6 @@
-dotnet publish .\AdapterLib\ -c Release -o lib_publish
-java -jar .\fitnesse-standalone.jar -d .\FitNesseRoot -f .\plugins.properties -p 8080 #-c "FrontPage.FirstLevelSuite?suite&format=text"
+dotnet publish .\AdapterLib\ -c Release -r win-x86 -o lib_publish
+docker build -t fitnesse:latest .
+"====================== In Container Run ======================"
+docker run --rm -v "$(Resolve-Path ./FitNesseRoot):C:/work/Fitnesse_Wiki" fitnesse -c "FrontPage.FirstLevelSuite?suite&format=text"
+"====================== Outside Run ==========================="
+java -jar .\fitnesse-standalone.jar -d .\FitNesseRoot -f .\plugins.properties -c "FrontPage.FirstLevelSuite?suite&format=text"
