56c7d3b264452568368c6a417385d338fec18eab
Anton Herzog <info@antonherzog.com>
2021-01-15 16:02:50 +0100

    Working dotnet5.0-windows with runtime in container and wpf in adapter.

--- AdapterLib/Class1.cs
+++ AdapterLib/Class1.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using Microsoft.Win32;
 
 namespace AdapterLib
 {
@@ -10,6 +15,38 @@ namespace AdapterLib
         public double Mul => A * B;
         public double Add => A + B;
         public double Sub => A - B;
-        public double Div => A / B;
+        public double Div
+        {
+            get
+            {
+                GetLoggingPath();
+                GetWpfControl();
+                return A / B;
+            }
+        }
+
+        private Control GetWpfControl()
+        {
+            Control c = new Button();
+            return c;
+        }
+
+        private static string GetLoggingPath()
+        {
+            // Verify the code is running on Windows.
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                using (var key = Registry.CurrentUser.OpenSubKey(@"Software\Fabrikam\AssetManagement"))
+                {
+                    if (key?.GetValue("LoggingDirectoryPath") is string configuredPath)
+                        return configuredPath;
+                }
+            }
+
+            // This is either not running on Windows or no logging path was configured,
+            // so just use the path for non-roaming user-specific data files.
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return Path.Combine(appDataPath, "Fabrikam", "AssetManagement", "Logging");
+        }
     }
 }
